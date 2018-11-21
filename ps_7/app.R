23f2eb0ed29aa4793f4fa6d0700b4ddf990f7a52
Maclaine Fields <maclainefields@Maclaines-MacBook-Pro.local>
2018-11-20 23:24:17 -0800

    adding scatter plot and data table to shiny app

--- ps_7/app.R
+++ ps_7/app.R
@@ -7,41 +7,88 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(shinythemes)
+library(plotly)
+
+
+#Reading in rds file shiny_data.
+dataset <- read_rds("shiny_data.rds")
+
+
+# Define UI for application
+ui <- fluidPage(theme = shinytheme("superhero"),
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("How Key Demographics Affect Forecasts of Republican Advantage"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar with a select input for demographic variable.  
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         selectInput("demographic",
+                     "Demographic:",
+                     choices = dataset$demographic), 
+         
+         #Add checkbox for the user to decide whether or not to add linear model. Note: This line of code is from Ms. Gayton's code.
+         checkboxInput("line", label = "Add Linear Model")
+       
       ),
       
-      # Show a plot of the generated distribution
+      # Add tabs. Show a plot of the generated scatter plots in the "Plot" tab. Show summary text in "summary" tab. 
       mainPanel(
-         plotOutput("distPlot")
+        
+        tabsetPanel(type = "tabs",
+                    tabPanel("Summary", 
+                             h1("Summary"),
+                             p("This Shiny App was created  using a collection of 2018 Midterm Election polling data from The Upshot. Additionally, thank you to Mr.
+Schroeder for the results dataset. The plots in this shiny app illustrate the relationship between the sample demographic in a polling district and the Upshot's predicted Republican Advantage."), 
+                             p("The analysis shows that there is a negative relationship between the percentage of young voters (18 to 29) in the sample and the forecasted Republican Advantage. A higher percentage of young voters in a polling district is 
+associated with a smaller forecasted Republican Advantage. Given that a large portion of voters between the ages of 18 and 29 are Democratic, this is not suprising."), 
+                             p("There also is a negative relationship between the percentage of college graduates in a polling district sample and the forecasted Republican Advantage. Given political trends, this is also not suprising."),
+                             p("There is a positive relationship between the percentage of female respondents in a polling district sample and the predicted republican advantage, meaning that a larger percentage of female 
+respondents in a polling district is associated with a higher predicted Republican Advantage."), 
+                             p("Interestingly, there is a positive relationship between the percentage of Black voters in a polling district sample and the predicted Republican Advantage. The relationship between the percentage of hispanic voters in the sample 
+and the predicted Republican Advantage is near zero.")
+                             ),
+                    tabPanel("Plot", plotOutput("demographics")),
+                    tabPanel("Data Table", dataTableOutput("datatable")))
+         
       )
    )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# Define server logic
+server <- function(input, output, session) {
+  
+  #Note: Figured out how to add linear model by looking at Ms. Gayton's Code.
+   output$demographics <- renderPlot({
+     
+     #Creates a filtered dataset based on the demographic that is chose in the corresponding selector in the sidepanel. 
+     filtered <- reactive({
+       df<- dataset %>% filter(demographic == input$demographic) 
+     })
+
+     #Creates scatterplot of demographic percentages and the forecasted republican advantage. I use the ifelse commands to show a linear model only if the corresponding checkbox is selected. 
+     if (input$line == FALSE) {
+       ggplot(filtered(), aes(x = value, y = forecast)) + geom_point() + xlab("Demographic (Percent)") + ylab("Forecasted Republican Advantage") + 
+       ggtitle("Respondent Demographics and Forecasted Republican Advantage")
+
+       }
+     else {
+       ggplot(filtered(), aes(x=value, y=forecast)) + geom_point() + geom_smooth(inherit.aes = FALSE, aes_string(x =filtered()$value, y =filtered()$forecast), method = "lm", se = FALSE) +
+       xlab("Demographic (Percent)") + ylab("Forecasted Republican Advantage") + ggtitle("Respondent Demographics and Forecasted Republican Advantage")
+     }
+     
+   })
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$datatable <- renderDataTable({
+     filtered <- reactive({
+       df<- dataset %>% filter(demographic == input$demographic) 
+     })
+     filtered()
    })
 }
 
